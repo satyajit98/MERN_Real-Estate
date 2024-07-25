f6d783b0659d27b8083f139934b47e70ba5540c6
SATYAJIT BHUNIA <mesatya2000@gmail.com>
2024-07-25 11:40:04 +0530

    create signIn api route and use jwt

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,7 +2,16 @@ const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const validator = require("validator");
 const errorHandler = require("../utils/error");
+const jwt = require("jsonwebtoken");
 
+//create token
+const createToken = (_id, username, email) => {
+  return jwt.sign({ _id, username, email }, process.env.SECRET, {
+    expiresIn: "3d",
+  });
+};
+
+//Signup
 const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -28,4 +37,31 @@ const signUp = async (req, res, next) => {
   }
 };
 
-module.exports = { signUp };
+const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    //find user
+    const isUser = await User.findOne({ email });
+    if (!isUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    //compare password
+    const user = bcrypt.compare(password, isUser.password);
+    if (!user) {
+      return next(errorHandler(401, "Wrong credentials!"));
+    }
+    //token create
+    const token = createToken(isUser._id, isUser.username, isUser.email);
+
+    //hiding password
+    const { password: pass, ...rest } = isUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, { httpOnly: true })
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { signUp, signIn };
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { signUp } = require("../controllers/auth.controller");
+const { signUp, signIn } = require("../controllers/auth.controller");
 
 const router = express.Router();
 
 router.post("/signup", signUp);
+router.post("/signin", signIn);
 
 module.exports = router;
