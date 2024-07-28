cc59a99298d7875f4646b112549c1c1537c092c7
SATYAJIT BHUNIA <mesatya2000@gmail.com>
2024-07-28 10:51:02 +0530

    add profile page UI

--- frontend/src/components/PrivateRoutes.jsx
+++ frontend/src/components/PrivateRoutes.jsx
@@ -3,5 +3,5 @@ import { Navigate, Outlet } from "react-router-dom";
 
 export default function PrivateRoutes() {
   const { currentUser } = useSelector((state) => state.user);
-  return currentUser ? <Outlet /> : <Navigate to={"/sign-in"} />;
+  return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
 }
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,42 @@
-import React from "react";
+import { useSelector } from "react-redux";
 
 export default function Profile() {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.photo}
+          alt="profile"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          className="rounded-lg p-3 border"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="rounded-lg p-3 border"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="rounded-lg p-3 border"
+          id="password"
+        />
+        <button className="bg-slate-600 text-white rounded-lg p-3 uppercase hover:opacity-85 disabled:opacity-95">
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">Sign Out</span>
+      </div>
+    </div>
+  );
 }
