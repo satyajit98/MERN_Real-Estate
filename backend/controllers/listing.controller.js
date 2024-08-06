828cd13e00b75a405059d1d72b153b59ff236583
SATYAJIT BHUNIA <mesatya2000@gmail.com>
2024-08-06 19:48:06 +0530

    complete delete user listing functionality

--- backend/controllers/listing.controller.js
+++ backend/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 const Listing = require("../models/listing.model");
+const errorHandler = require("../utils/error");
 
 const createListing = async (req, res, next) => {
   try {
@@ -9,4 +10,22 @@ const createListing = async (req, res, next) => {
   }
 };
 
-module.exports = createListing;
+const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only delete your own listings!"));
+  }
+  try {
+    const listingDelete = await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json("Listing deleted");
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { createListing, deleteListing };
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -143,6 +143,25 @@ export default function Profile() {
     }
   };
 
+  // delete listing
+  const handleDeleteListing = async (listingId) => {
+    try {
+      const response = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await response.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListing((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -259,7 +278,12 @@ export default function Profile() {
                 <p>{listing.name}</p>
               </Link>
               <div className=" flex flex-col items-center">
-                <button className="text-red-700 uppercase">Delete</button>
+                <button
+                  onClick={() => handleDeleteListing(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
                 <button className="text-green-700 uppercase">Edit</button>
               </div>
             </div>
