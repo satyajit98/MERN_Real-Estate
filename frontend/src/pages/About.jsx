283b22d8f23e8f6469715c072f053a644b1957e8
SATYAJIT BHUNIA <mesatya2000@gmail.com>
2024-08-15 19:26:10 +0530

    complete about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,5 +1,30 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Satyajit Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Satyajit Estate is a leading real estate platform that specializes in
+        helping clients buy, sell, and rent properties in the most desirable
+        neighborhoods. My platform is dedicated to providing exceptional service
+        and making the buyind and selling process as smooth as possible.
+      </p>
+      <p className="mb-4 text-slate-700">
+        My mission is to help our clients achieve their real estate goals by
+        providing expert advice, personalized service, and a deep understanding
+        of the local market. Weather you are looking to buy, sell, of rent a
+        property, I am here to help you every step of the way.
+      </p>
+      <p className="mb-4 text-slate-700">
+        My platform has wealth of experience and knowledge in the real estate
+        industry, and i am committed to providing the highest level of service
+        to our clients. I believe that buying or selling a property should be an
+        exciting and rewarding experience, and I am dedicated to making that
+        reality for each and every one of our clients.
+      </p>
+    </div>
+  );
 }
